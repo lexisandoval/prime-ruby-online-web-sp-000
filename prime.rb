1e16f0a53f4639455f87869d7825aafea993427e
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-01-15 17:53:37 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(number)
+  if number <= 1
+    return false
+  end
+
+  i = 2
+  until i == number
+    if number % i == 0
+      return false
+    end
+    i += 1
+  end
+  return true
+end
